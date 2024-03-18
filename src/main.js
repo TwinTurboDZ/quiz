ff0c5601596922a145ecf5f5e286b8e593142fc6
Belhanafi Abdelmadjid <112507960+BelhanafiAbdelmadjid@users.noreply.github.com>
2024-03-18 22:46:32 +0100

    forgot

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import routes from './route'
+import { createRouter, createWebHistory } from 'vue-router'
+import { createPinia } from 'pinia'
+import mitt from 'mitt'
+
+const emitter = mitt();
+const pinia = createPinia()
+const router = createRouter({
+    history: createWebHistory('/quiz/'),
+    routes, // short for `routes: routes`
+})
+
+const app = createApp(App);
+
+app.config.globalProperties.emitter = emitter;
+app.use(router);
+app.use(pinia);
+app.mount('#app');
